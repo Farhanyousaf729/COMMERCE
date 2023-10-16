efc61dc9b3ededc377c0f336766f04b1680bbe39
Farhanyousaf729 <fboby23@gmail.com>
2023-10-16 21:43:36 +0500

    cors

--- backend/features/cors.js
+++ backend/features/cors.js
@@ -2,6 +2,7 @@ const whiteList = [
     'https://yoursite.com',
     'http://localhost:4000/',
     'http://localhost:3500/',
+    'https://commerce-farhanyousaf729.vercel.app/'
 ]
 
 const corsOptions = {
