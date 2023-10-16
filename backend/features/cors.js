86f2184ca202b164e78256d39c54b21bd44ba367
Farhanyousaf729 <fboby23@gmail.com>
2023-10-16 15:43:23 +0500

    issue

--- backend/features/cors.js
+++ backend/features/cors.js
@@ -1,7 +1,9 @@
 const whiteList = [
     'https://yoursite.com',
     'http://localhost:4000/',
-    'http://localhost:3500/'
+    'http://localhost:3500/',
+    "https://puce-determined-blackbuck.cyclic.app"
+
 ]
 
 const corsOptions = {
